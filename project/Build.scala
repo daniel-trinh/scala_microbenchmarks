fac32b8e68844f6ec966ab2b9135cf7aa4ec3bb7
Daniel Trinh <me@daniel.trinh>
2013-09-19 22:44:20 -0700

    added a try vs future benchmark

---
+++ project/Build.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+
+  lazy val project = Project("root", file(".")).settings(
+
+    // basics
+    name := "scala-benchmarks",
+    organization := "com.danieltrinh",
+    version := "0.1.0",
+    scalaVersion := "2.10.2",
+
+    // dependencies
+    libraryDependencies ++= Seq(
+        "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
+        "com.google.code.gson" % "gson" % "1.7.1"
+    ),
+    resolvers += "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+
+    // enable forking in run
+    fork in run := true,
+
+    // we need to add the runtime classpath as a "-cp" argument to the `javaOptions in run`, otherwise caliper
+    // will not see the right classpath and die with a ConfigurationException
+    javaOptions in run <++= (fullClasspath in Runtime) map { cp => Seq("-cp", sbt.Build.data(cp).mkString(":")) }
+  )
+}
---
+++ src/main/scala/com/danieltrinh/benchmarks/TryVsFuture.scala
@@ -0,0 +1,102 @@
+package com.danieltrinh.benchmarks
+
+import org.example.SimpleScalaBenchmark
+import com.google.caliper.Param
+import scala.concurrent.Future
+import scala.util.{ Try, Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TryVsFuture extends SimpleScalaBenchmark {
+
+  @Param(Array("10", "100", "1000"))
+  val length: Int = 0
+
+  var array: Array[Int] = _
+
+
+  override def setUp() {
+    array = new Array(length)
+  }
+
+
+  def timeTrySuccess(reps: Int): Try[Int] = repeat(reps) {
+
+    var i = 0
+    var result = Success(i)
+    while(i < array.length) {
+      result = Success(i)
+      i = i + 1
+    }
+    result
+  }
+  def timeTryFailure(reps: Int): Try[Int] = repeat(reps) {
+    val e = new RuntimeException("Yep")
+    var i = 0
+    var result = Failure(e)
+    while(i < array.length) {
+      result = Failure(e)
+      i = i + 1
+    }
+    result
+  }
+
+  // This 30-60 times slower than Future.successful.. leaving it out 
+  // since it is so slow
+  // def timeFuture(reps: Int): Future[Int] = repeat(reps) {
+
+  //   var i = 0
+  //   var result = Future(i)
+  //   while(i < array.length) {
+  //     result = Future(i)
+  //     i = i + 1
+  //   }
+  //   result
+  // }
+
+  def timeFutureSuccessful(reps: Int): Future[Int] = repeat(reps) {
+
+    var i = 0
+    var result = Future.successful(i)
+    while(i < array.length) {
+      result = Future.successful(i)
+      i = i + 1
+    }
+    result
+
+  }
+  def timeFutureFailed(reps: Int): Future[Int] = repeat(reps) {
+    val e = new RuntimeException("Yep")
+
+    var i = 0
+    var result = Future.failed[Int](e)
+    while(i < array.length) {
+      result = Future.failed[Int](e)
+      i = i + 1
+    }
+    result
+
+  }
+
+  def timeFutureSuccessTrySuccess(reps: Int): Future[Try[Int]] = repeat(reps) {
+
+    var i = 0
+    var result = Future.successful(Success(i))
+    while(i < array.length) {
+      result = Future.successful(Success(i))
+      i = i + 1
+    }
+    result
+  }
+
+  def timeOptionSome(reps: Int): Option[Int] = repeat(reps) {
+
+    var i = 0
+    var result = Some(i)
+    while(i < array.length) {
+      result = Some(i)
+      i = i + 1
+    }
+    result
+
+  }
+}
